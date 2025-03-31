c90ad10a74c5fc6f6ad2d14b6c81fd4e5bde0419
Ana Guimarães <ana.carolina.gui.ramos@gmail.com>
2025-03-31 16:23:33 -0300

    Função para converter reais para wons

--- aula01/index.js
+++ aula01/index.js
@@ -2,7 +2,8 @@ function conversor() {
     const umWon = 0.0040
     const valorWons = prompt("Digite um valor em won:")
     
-    // o parseFloat serve para exibir o valor com, nesse caso, duas casas decimais
+    // o parseFloat serve para exibir o valor com duas casas decimais
     const valorReais = umWon * parseFloat(valorWons) 
+    // toLocaleString --> Formata a string de acordo com o local informado no método
     alert("R$" + valorReais.toLocaleString("pt-BR"))
 }
